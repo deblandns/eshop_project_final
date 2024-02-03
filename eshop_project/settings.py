dd4a94a11b33896357d134a77ed4a071eed37c28
DEBLAND <hoseinnysyan1385@gmail.com>
2024-02-03 17:34:23 +0330

    add email smtp send sms for activation

--- eshop_project/settings.py
+++ eshop_project/settings.py
@@ -147,8 +147,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'onionarchitecturemvc@gmail.com'
-EMAIL_HOST_PASSWORD = 'djangotoplearn@pass'
+EMAIL_HOST_USER = 'eshopprojectdiardev@gmail.com'
+EMAIL_HOST_PASSWORD = 'kywnebshvrunwndv'
 EMAIL_PORT = 587
 
 # default settings
